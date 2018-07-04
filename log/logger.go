f770ab4a633bebfd0391efe786f9f197c0ffce32
gurparit <github@gurparit.com>
2018-07-04 21:42:30 +0100

    Added a logger and uuid generator to common.

---
+++ log/logger.go
@@ -0,0 +1,22 @@
+package log
+
+import (
+	"fmt"
+	"time"
+)
+
+func Println(message interface{}, args ...interface{}) {
+	var log interface{}
+
+	switch message.(type) {
+	case string:
+		log = fmt.Sprintf(message.(string), args...)
+		break
+	default:
+		log = message
+		break
+	}
+
+	timeStamp := time.Now().Format(time.RFC3339)
+	fmt.Printf("[%s] %v\n", timeStamp, log)
+}
